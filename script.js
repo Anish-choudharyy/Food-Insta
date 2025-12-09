e51b557da99082a604d915bce235169ca1cc972b
Anish-choudharyy <anish.kajla.2007@gmail.com>
2025-12-09 17:36:39 +0530

    Fix event listener for dark mode toggle

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ Btn.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
 
     if (document.body.classList.contains("dark-mode")) {
-        localStorage.addEventListener("darkMode", "enabled");
-    } else {
-        localStorage.addEventListener("darkMode", "disabled");
+        addEventListener("darkMode", "enabled");
+    } 
+    else {
+        addEventListener("darkMode", "disabled");
     }
 });
