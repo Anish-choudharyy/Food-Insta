9086c41182e8cbe537c1fbdad80067a1bb410b2d
Anish-choudharyy <anish.kajla.2007@gmail.com>
2025-12-09 18:18:44 +0530

    Remove unnecessary event listener logic

--- script.js
+++ script.js
@@ -2,11 +2,4 @@ let Btn = document.getElementById("themeToggle");
 
 Btn.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
-
-    if (document.body.classList.contains("dark-mode")) {
-        addEventListener("darkMode", "enabled");
-    } 
-    else {
-        addEventListener("darkMode", "disabled");
-    }
 });
